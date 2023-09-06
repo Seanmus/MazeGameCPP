a2cfea4f2f52c73c7e6fed7ed9a37bf013a5eb8b
Sean Piche <spiche91@academic.rrc.ca>
2023-09-06 14:45:04 -0500

    Fixed player not compiling and created bp derived from player and from gamemode

---
+++ Source/MazeGame/PlayerCharacter.cpp
@@ -0,0 +1,132 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerCharacter.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "MazeGameInstance.h"
+#include "MazeGameGameModeBase.h"
+
+using UEILPS = UEnhancedInputLocalPlayerSubsystem;
+using UEIC = UEnhancedInputComponent;
+
+// Sets default values
+APlayerCharacter::APlayerCharacter() {
+
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Cube = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Cube"));
+	Cube->SetSimulatePhysics(true);
+	RootComponent = Cube;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(Cube);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+	PlayerSpawn = Cube->GetComponentLocation();
+
+}
+
+// Called when the game starts or when spawned
+void APlayerCharacter::BeginPlay() {
+	Super::BeginPlay();
+	GameMode = CastChecked<AMazeGameGameModeBase>(UGameplayStatics::GetGameMode(GetWorld()));
+	if (GameMode)
+	{
+	}
+
+
+
+	if (const APlayerController* PlayerController = Cast<APlayerController>(GetController())) {
+		const ULocalPlayer* LocalPlayer = PlayerController->GetLocalPlayer();
+		if (UEILPS* SubSystem = ULocalPlayer::GetSubsystem<UEILPS>(LocalPlayer)) {
+			if (InputMappingContext) {
+				SubSystem->AddMappingContext(InputMappingContext, 0);
+			}
+			else {
+
+			}
+		}
+	}
+
+	if (Cube) {
+		Cube->OnComponentHit.AddDynamic(this, &APlayerCharacter::OnHit);
+		Cube->OnComponentBeginOverlap.AddDynamic(this, &APlayerCharacter::OnBeginOverlap);
+	}
+}
+
+// Called every frame
+void APlayerCharacter::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+
+	if (!bLevelEnded) {
+		const FVector CubeForce = FVector(ForwardForce, 0.0f, 0.0f);
+		Cube->AddForce(CubeForce, NAME_None, true);
+		if (Cube->GetComponentLocation().Z < KillZ)
+		{
+			PlayerDied();
+		}
+	}
+}
+
+// Called to bind functionality to input
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (UEIC* EnhancedInputComponent = CastChecked<UEIC>(PlayerInputComponent)) {
+		if (MoveAction) {
+			EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this,
+				&APlayerCharacter::MoveRightLeft);
+		}
+	}
+}
+
+void APlayerCharacter::MoveRightLeft(const FInputActionValue& Value) {
+	const float MovementAxis = Value.Get<float>();
+
+	if (!bLevelEnded) {
+		const FVector CubeForce = FVector(0.0f, MovementAxis * SideForce, 0.0f);
+		Cube->AddForce(CubeForce, NAME_None, true);
+	}
+}
+
+void APlayerCharacter::PlayerDied()
+{
+	bLevelEnded = true;
+	Cube->SetPhysicsLinearVelocity({ 0, 0, 0 });
+	GetWorldTimerManager().SetTimer(PlayerDiedTimer, [this]() {GameMode->GameCompleted(false); }, 2.0f, false);
+	//GetWorldTimerManager().SetTimer(PlayerDiedTimer,[this](bool bGameOver) {GameMode->GameCompleted(bGameOver); }, 2.0f, false);
+	//GetWorldTimerManager().SetTimer(PlayerDiedTimer, this, &APlayerCharacter::GameOver, 2.0f);
+	//GameMode->GameCompleted(false);
+	//Cube->SetWorldLocationAndRotation(PlayerSpawn, FRotator(0, 0, 0));
+	//Cube->SetWorldLocation(PlayerSpawn);
+}
+
+
+
+void APlayerCharacter::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent,
+	FVector NormalImpulse, const FHitResult& Hit) {
+	//if (OtherActor && OtherActor->IsA(AObstacle::StaticClass())) {
+	//	PlayerDied();
+	//}
+}
+
+void APlayerCharacter::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
+	//if (OtherActor && OtherActor->IsA(AEndPoint::StaticClass())) {
+	//	bLevelEnded = true;
+	//	Cube->SetPhysicsLinearVelocity(Cube->GetPhysicsLinearVelocity() * 0.9f);
+	//	GameMode->LevelCompleted();
+	//}
+}
+
+void APlayerCharacter::GameOver()
+{
+	GameMode->GameCompleted(false);
+}
+
+
---
+++ Source/MazeGame/PlayerCharacter.h
@@ -0,0 +1,92 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
+#include "MazeGameGameModeBase.h"
+#include "MazeGameInstance.h"
+#include "PlayerCharacter.generated.h"
+
+
+// Forward Declarations:
+class UInputMappingContext;
+class UInputAction;
+class USpringArmComponent;
+class UCameraComponent;
+
+
+UCLASS()
+class MAZEGAME_API APlayerCharacter : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// ENHANCED INPUT SYSTEM
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputMappingContext* InputMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* MoveAction;
+
+	void MoveRightLeft(const FInputActionValue& Value);
+
+	// ASSET COMPONENTS
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Setup)
+	UStaticMeshComponent* Cube;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Setup)
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Setup)
+	UCameraComponent* Camera;
+
+	// MOVEMENT VARIABLES
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float ForwardForce = 1800.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float SideForce = 1800.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	bool bLevelEnded = false;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float KillZ = -1000;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	FVector PlayerSpawn;
+
+	UPROPERTY()
+	AMazeGameGameModeBase* GameMode;
+
+	FTimerHandle PlayerDiedTimer;
+
+	void PlayerDied();
+
+	UFUNCTION() // Required to bind to dynamic multi-cast delegate.
+		void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+
+	UFUNCTION() // The names of these functions don't matter, but the signature does.
+		void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void GameOver();
+};
